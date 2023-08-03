9177a068b0a708568e3279295e5464b8c38cc91c
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2023-08-03 18:15:49 +0530

    ADDED: Method to handle global exceptions

--- src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
+++ src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
@@ -11,7 +11,7 @@ import java.util.Date;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
+    // handle custom exception
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest webRequest) {
         ErrorDetails details = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
@@ -23,6 +23,15 @@ public class GlobalExceptionHandler {
         ErrorDetails details = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(details, HttpStatus.NOT_FOUND);
     }
+
+    // handle global exception
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleGlobalException(Exception exception, WebRequest webRequest) {
+        ErrorDetails details = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(details, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
 
 
 }

a015366f52d154e5520a685da6dfe833cbf48328
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2023-08-03 12:29:01 +0530

    ADDED: Implementation for method in comment repository

--- src/main/java/com/springboot/blog/service/impl/CommentServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/CommentServiceImpl.java
@@ -3,12 +3,14 @@ package com.springboot.blog.service.impl;
 import com.springboot.blog.configuration.ModelMapperConfiguration;
 import com.springboot.blog.entity.Comment;
 import com.springboot.blog.entity.Post;
+import com.springboot.blog.exception.BlogAPIException;
 import com.springboot.blog.exception.ResourceNotFoundException;
 import com.springboot.blog.payload.CommentDto;
 import com.springboot.blog.repository.CommentRepository;
 import com.springboot.blog.repository.PostRepository;
 import com.springboot.blog.service.CommentService;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -47,15 +49,24 @@ public class CommentServiceImpl implements CommentService {
                 .stream()
                 .map((comment) -> modelMapper.map(comment, CommentDto.class))
                 .collect(Collectors.toList());
+
+
     }
 
     @Override
     public CommentDto getCommentByPostIdAndCommentId(long postId, long commentId) {
-         var Comment = commentRepository
-                        .findByCommentIdAndPostId(postId, commentId)
-                        .orElseThrow(()->new ResourceNotFoundException("Resource not found with given comment id or post","id", commentId));
-         return modelMapper.map(Comment, CommentDto.class);
+//        var Comment = commentRepository
+//                .findByCommentIdAndPostId(postId, commentId)
+//                .orElseThrow(() -> new ResourceNotFoundException("Resource not found with given comment id or post", "id", commentId));
+//        return modelMapper.map(Comment, CommentDto.class);
+
+        Post post = postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException("Post", "id", postId));
+
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new ResourceNotFoundException("Comment", "id", commentId));
+
+        if (!comment.getPost().getId().equals(post.getId())) {
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Comment does not belong to post");
+        }
+        return modelMapper.map(comment, CommentDto.class);
     }
-
-
 }

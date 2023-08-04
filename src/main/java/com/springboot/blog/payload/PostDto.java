b308353d79b11f5790a313cb3ee5327139d0da5a
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2023-08-04 15:44:35 +0530

    ADDED: Validations for fields using annotations

--- src/main/java/com/springboot/blog/payload/PostDto.java
+++ src/main/java/com/springboot/blog/payload/PostDto.java
@@ -1,5 +1,8 @@
 package com.springboot.blog.payload;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -12,8 +15,13 @@ import java.util.Set;
 @Data
 public class PostDto implements Serializable {
     private Long id;
+    @NotEmpty
+    @Size(min = 2, message = "Post title should have at least 2 characters")
     private String title;
+    @NotEmpty
+    @Size(min = 10, message = "Post description should have at least 10 characters")
     private String description;
+    @NotEmpty
     private String content;
     private Set<CommentDto> comments;
 }

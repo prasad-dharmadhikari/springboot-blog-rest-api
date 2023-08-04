e9f6b29df19e5a3613e31da46b2702c0e01b9138
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2023-08-04 17:44:58 +0530

    REFACTORED: Added validations for fields in CommentDTO

--- src/main/java/com/springboot/blog/payload/CommentDto.java
+++ src/main/java/com/springboot/blog/payload/CommentDto.java
@@ -4,6 +4,7 @@ import com.springboot.blog.entity.Comment;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,7 +21,8 @@ public class CommentDto implements Serializable {
 
     private Long id;
 
-    @NotEmpty
+    @NotEmpty(message = "Name should not be null or empty")
+    @Size(min = 2,message = "Name should be at least of 2 characters")
     private String name;
 
     @NotNull(message = "Email should not be null")
@@ -28,6 +30,7 @@ public class CommentDto implements Serializable {
     @NotEmpty(message = "Email id should not be empty")
     private String email;
 
-    @NotNull(message = "Body should not be null")
+    @NotEmpty(message = "Body should not be null")
+    @Size(min = 10, message = "Body should have at least 10 characters")
     private String body;
 }

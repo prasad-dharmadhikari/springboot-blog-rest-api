34297f708b65b11580afd3ed907c3355684bd9b0
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2023-08-08 18:34:26 +0530

    ADDED: code for in-memory authentication

--- src/main/java/com/springboot/blog/configuration/SecurityConfig.java
+++ src/main/java/com/springboot/blog/configuration/SecurityConfig.java
@@ -4,15 +4,34 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable().
                 authorizeHttpRequests(authorized -> authorized.anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetails() {
+        UserDetails prasad = User.builder().username("Prasad").password(passwordEncoder().encode("Prasad123")).roles("USER").build();
+        UserDetails admin = User.builder().username("Admin").password(passwordEncoder().encode("Admin123")).roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(prasad, admin);
     }
 }

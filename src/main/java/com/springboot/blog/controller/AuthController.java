f33c55ab2deb09536e93aa8e31bc02e0b0daca77
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2023-08-09 16:47:08 +0530

    ADDED: Controller for login endpoint

---
+++ src/main/java/com/springboot/blog/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.springboot.blog.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.springboot.blog.payload.LoginDto;
+import com.springboot.blog.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth/")
+public class AuthController {
+
+    private AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    // Build login rest api
+    @PostMapping(value = {"login","sign-in"})
+    public ResponseEntity<ObjectNode> login(@RequestBody LoginDto loginDto) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        var objectNode = objectMapper.createObjectNode();
+        objectNode.put("message", authService.login(loginDto));
+        return ResponseEntity.ok(objectNode);
+    }
+}

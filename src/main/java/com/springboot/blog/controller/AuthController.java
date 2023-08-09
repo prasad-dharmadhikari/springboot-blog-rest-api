726ff58dffdf7b1ae78f4390dabc8b44b31dece6
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2023-08-09 18:18:21 +0530

    REFACTORED: Rest endpoint for register API

--- src/main/java/com/springboot/blog/controller/AuthController.java
+++ src/main/java/com/springboot/blog/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.springboot.blog.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.springboot.blog.payload.LoginDto;
+import com.springboot.blog.payload.RegisterDto;
 import com.springboot.blog.service.AuthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,4 +31,14 @@ public class AuthController {
         objectNode.put("message", authService.login(loginDto));
         return ResponseEntity.ok(objectNode);
     }
+    // Build Register rest api
+    @PostMapping(value = {"register","sign-up"})
+    public ResponseEntity<ObjectNode> register(@RequestBody RegisterDto registerDto) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        var objectNode = objectMapper.createObjectNode();
+        objectNode.put("message", authService.register(registerDto));
+        return ResponseEntity.ok(objectNode);
+    }
+
+
 }

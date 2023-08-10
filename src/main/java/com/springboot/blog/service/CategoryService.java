7f9926b492abd716d13d1ed318fa16263a50fe34
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2023-08-10 17:13:45 +0530

    ADDED: Service interface for category

---
+++ src/main/java/com/springboot/blog/service/CategoryService.java
@@ -0,0 +1,11 @@
+package com.springboot.blog.service;
+
+import com.springboot.blog.payload.CategoryDto;
+
+public interface CategoryService {
+    CategoryDto addCategory(CategoryDto categoryDto);
+    CategoryDto getCategoryById(Long categoryId);
+
+
+
+}

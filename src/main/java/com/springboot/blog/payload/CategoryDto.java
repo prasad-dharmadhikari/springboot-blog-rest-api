7cc740b7a4a4b7b35c6b2c5354be5a1bb341dffa
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2023-08-10 17:12:28 +0530

    ADDED: DTO for category

---
+++ src/main/java/com/springboot/blog/payload/CategoryDto.java
@@ -0,0 +1,31 @@
+package com.springboot.blog.payload;
+
+import com.springboot.blog.entity.Category;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.io.Serializable;
+
+/**
+ * DTO for {@link Category}
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDto implements Serializable {
+
+    private Long id;
+
+    @NotNull(message = "Name should not be null")
+    @Size(min = 3)
+    @NotEmpty(message = "Name should not be empty")
+    private String name;
+
+    @NotNull(message = "Description should not be null")
+    @Size(min = 10)
+    @NotEmpty(message = "Description should not be empty")
+    private String description;
+}

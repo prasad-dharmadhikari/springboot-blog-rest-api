e1f5b9a5fc8e88e4dbc897d5aa58077350b542c8
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2023-08-10 13:42:39 +0530

    REFACTORED: Code to configure JWTAuthentication filter before any authentication

--- src/main/java/com/springboot/blog/configuration/SecurityConfig.java
+++ src/main/java/com/springboot/blog/configuration/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.springboot.blog.configuration;
 
+import com.springboot.blog.security.JwtAuthenticationEntryPoint;
+import com.springboot.blog.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +12,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,16 +20,25 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
 
 
-    private UserDetailsService userService;
+    private UserDetailsService userDetailsService;
 
-    public SecurityConfig(UserDetailsService userDetailsService) {
-        this.userService = userDetailsService;
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(UserDetailsService userDetailsService,
+                          JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+                          JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.userDetailsService = userDetailsService;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
 
     @Bean
@@ -37,14 +49,15 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable().
-                authorizeHttpRequests(
-                        // Permission for all users to access to all GET Endpoints
-                        authorized -> authorized.requestMatchers(HttpMethod.GET,"/api/**").permitAll()
-                                                .requestMatchers("/api/auth/**").permitAll()
-                                // Apart from above methods, all requests should be authenticated
-                                .anyRequest().authenticated()
-                )//authorized.anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults());
+                authorizeHttpRequests(authorized ->
+                        authorized.requestMatchers(HttpMethod.GET,"/api/**").permitAll()
+                                  .requestMatchers("/api/auth/**").permitAll()
+                                  .anyRequest().authenticated()
+                ).exceptionHandling(exception ->
+                                    exception.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+                .sessionManagement(session ->
+                                   session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
     @Bean

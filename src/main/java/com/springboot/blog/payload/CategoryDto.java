03a5b2a92721c4d2f702919332b1c7874d593ba5
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2023-08-11 13:15:34 +0530

    ADDED: Validations for fields

--- src/main/java/com/springboot/blog/payload/CategoryDto.java
+++ src/main/java/com/springboot/blog/payload/CategoryDto.java
@@ -20,12 +20,12 @@ public class CategoryDto implements Serializable {
     private Long id;
 
     @NotNull(message = "Name should not be null")
-    @Size(min = 3)
+    @Size(min = 2, message = "Description should have at least 2 characters")
     @NotEmpty(message = "Name should not be empty")
     private String name;
 
     @NotNull(message = "Description should not be null")
-    @Size(min = 10)
+    @Size(min = 10, message = "Description should have at least 2 characters")
     @NotEmpty(message = "Description should not be empty")
     private String description;
 }

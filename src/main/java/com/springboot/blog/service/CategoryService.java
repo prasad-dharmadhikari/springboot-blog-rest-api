611a9df6d3d8e0afe079d90102b8a2dc7a3042cc
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2023-08-11 13:16:03 +0530

    ADDED: Method for deleting a category

--- src/main/java/com/springboot/blog/service/CategoryService.java
+++ src/main/java/com/springboot/blog/service/CategoryService.java
@@ -2,10 +2,12 @@ package com.springboot.blog.service;
 
 import com.springboot.blog.payload.CategoryDto;
 
+import java.util.List;
+
 public interface CategoryService {
     CategoryDto addCategory(CategoryDto categoryDto);
     CategoryDto getCategoryById(Long categoryId);
-
-
-
+    List<CategoryDto> getAllCategories();
+    CategoryDto updateCategory(CategoryDto categoryDto, Long categoryId);
+    void removeCategory(Long categoryId);
 }

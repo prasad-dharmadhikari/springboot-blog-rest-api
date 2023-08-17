579354aba5c894609eb22e0cedea48a906250752
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2023-08-17 13:20:27 +0530

    ADDED: Open API Definition with some information

--- src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
+++ src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
@@ -1,11 +1,34 @@
 package com.springboot.blog;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring Boot Blog App REST APIs",
+				description = "Spring Boot Blog App REST APIs Documentation",
+				version ="v1.0",
+				contact = @Contact(
+						name ="Prasad Dharmadhikari",
+						email = "prasad.dharmadhikari4669@gmail.com",
+						url = "https://github.com/prasad-dharmadhikari/springboot-blog-rest-api"
+				),
+				license = @License(
+						name = "Apache License, Version 2.0",
+						url = "https://github.com/prasad-dharmadhikari/springboot-blog-rest-api"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Spring Boot Blog application documentation",
+				url = "https://github.com/prasad-dharmadhikari/springboot-blog-rest-api"
+		)
+)
 public class SpringbootBlogRestApiApplication {
 
 	public static void main(String[] args) {

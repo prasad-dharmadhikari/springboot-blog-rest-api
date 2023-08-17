b2096a15d717ab9901387db549d104a0c9b908b1
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2023-08-17 12:39:05 +0530

    REFACTORED: Method to handle invalid argument exception

--- src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
+++ src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.server.handler.ResponseStatusExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -66,5 +67,12 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         ErrorDetails details = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(details, HttpStatus.UNAUTHORIZED);
     }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorDetails> handleBadCredentialsException(MethodArgumentTypeMismatchException exception, WebRequest webRequest) {
+        ErrorDetails details = new ErrorDetails(new Date(), "Argument Type Mismatch, enter argument with required data type", webRequest.getDescription(false));
+        return new ResponseEntity<>(details, HttpStatus.BAD_REQUEST);
+    }
+
 
 }
